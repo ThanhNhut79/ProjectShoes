b81895673c0696fe4c5dffa2dcb9c63ee2016a5e
thaihuanlx <thaihuanlx@gmail.com>
2024-01-07 19:36:03 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <Footer />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
