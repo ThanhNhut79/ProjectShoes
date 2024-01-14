cb1f61f1ce6f1acb58478dc7b8d226d51b995bf8
ThanhNhut79 <thanhnhutvn77@gmail.com>
2024-01-14 19:09:00 +0700

    update

--- src/Pages/Product/ComponentProduct/FilterList.jsx
+++ src/Pages/Product/ComponentProduct/FilterList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const FilterList = ({
   searchTerm,
@@ -13,30 +14,36 @@ const FilterList = ({
 }) => (
   <>
     <div className="filter-container">
-      <label>Search: </label>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
+      <div className="filter-productpage">
+        <label>Search: </label>
+        <div className="search-product">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button onClick={handleSearch}>
+            <FaSearch />
+          </button>
+        </div>
+        <div className="filter-price">
+          <label>Min Price: </label>
+          <input
+            type="number"
+            value={minPrice}
+            onChange={(e) => setMinPrice(e.target.value)}
+          />
+          <label>Max Price: </label>
+          <input
+            type="number"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(e.target.value)}
+          />
+          <button onClick={handleFilterByPrice}>Filter by Price</button>
+          <button onClick={handleResetFilters}>Reset Filters</button>
+        </div>
+      </div>
     </div>
-    <div className="filter-container">
-      <label>Min Price: </label>
-      <input
-        type="number"
-        value={minPrice}
-        onChange={(e) => setMinPrice(e.target.value)}
-      />
-      <label>Max Price: </label>
-      <input
-        type="number"
-        value={maxPrice}
-        onChange={(e) => setMaxPrice(e.target.value)}
-      />
-      <button onClick={handleFilterByPrice}>Filter by Price</button>
-    </div>
-    <button onClick={handleResetFilters}>Reset Filters</button>
   </>
 );
 
